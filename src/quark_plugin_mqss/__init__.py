b151754fde22e7a70b8835acc596d66954dbef78
Marvin Erdmann <Marvin.Erdmann@bmw.de>
2025-09-30 14:44:54 +0200

    Revert "Apply automatic changes"
    This reverts commit 04c6ac3fc1a6067d644bce4c66ed1a5345912fea.

--- src/quark_plugin_mqss/__init__.py
+++ src/quark_plugin_mqss/__init__.py
@@ -1,7 +1,11 @@
 from quark.plugin_manager import factory
 
+<<<<<<<< HEAD:src/quark_plugin_mqss/__init__.py
 from quark_plugin_mqss.circuit_provider import CircuitProvider
 from quark_plugin_mqss.job_execution import JobExecution
+========
+from PLUGIN_NAME_FORMATTED.example_module import ExampleModule
+>>>>>>>> parent of 04c6ac3 (Apply automatic changes):src/PLUGIN_NAME_FORMATTED/__init__.py
 
 def register() -> None:
     """
